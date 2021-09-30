1e8a6ce7bd28a23ed36429464106bce0e969f3d4
Anton Andersen <81985737+tishafeed@users.noreply.github.com>
2021-09-30 13:31:34 +0300

    Update part1_1.py

--- part1_1.py
+++ part1_1.py
@@ -1,5 +1,5 @@
 class Rectangle:
-    def __init__(self, length=1, width=1):
+    def __init__(self, length=1, width=1):  # 1 is the default for any unset field
         if 0 < length < 20:
             self.__length = length
         else:
@@ -9,32 +9,33 @@ class Rectangle:
         else:
             self.__width = 1
 
-    def __str__(self):
+    def __str__(self):  # redefined str() for convenience
         return 'length = ' + str(self.__length) + '\n' + 'width = ' + str(self.__width)
 
-    @property
+    @property  # length getter
     def length(self):
         return self.__length
 
-    @length.setter
+    @length.setter  # length setter
     def length(self, length1):
         if 0 < length1 < 20:
             self.__length = length1
 
-    @property
+    @property   # width getter
     def width(self):
         return self.__width
 
-    @width.setter
+    @width.setter  # width setter
     def width(self, width1):
         if 0 < width1 < 20:
             self.__width = width1
 
-    def area(self):
+    def area(self):  # area calculate
         return self.__width * self.__length
 
-    def perimeter(self):
+    def perimeter(self):  # area calculate
         return (self.__width + self.__length) * 2
+
 
 a = Rectangle()
 b = Rectangle(10, 5)
